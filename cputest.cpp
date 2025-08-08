54b9a73f6a1aa0fc4ed99ac40da370b6fc8f2dd5
voltage017 <voltage0177@gmail.com>
2025-08-08 15:34:23 +0300

    commit

---
+++ cputest.cpp
@@ -0,0 +1,9 @@
+// voltage017 (2025) (c) All Rights Reserved
+#include <iostream>
+using namespace std;
+int main(){
+int i = 0;
+        do {
+    cout << "CPU checker is active: " << i << endl;
+        } while (i < 10);
+    }
